4a6547e0ec223d65e5c16aba6d3c6e28eaa875b4
kevintom <kk860621kk@gmail.com>
2022-12-04 15:34:22 +0800

    callback

---
+++ js/callback.js
@@ -0,0 +1,9 @@
+let doWork = function (job, timer) {
+    setTimeout(() => {
+        console.log(`完成工作 ${job} at ${now.toISOString()}`, timer);
+    });
+};
+
+let now = new Date();
+console.log(`工作開始 at ${now.toISOString()}`);
+doWork('刷牙', 3000);
