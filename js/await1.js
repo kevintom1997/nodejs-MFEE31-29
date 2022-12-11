3259d45e154d8433d5ba14afcea480416584aa84
kevintom <kk860621kk@gmail.com>
2022-12-11 11:06:08 +0800

    [crawler]

--- js/await1.js
+++ js/await1.js
@@ -1,12 +1,3 @@
-// Promise 是一個表示非同步運算的最終完成或失敗的物件。
-// 1. Promise 是一個物件 new Promise()
-//    new Promise(executor)
-//    executor: function(resolve, reject) {}
-// 2. 執行非同步工作
-// 3. 最終完成或失敗
-//    完成 -> 呼叫 resolve
-//    失敗 -> 呼叫 reject
-
 let doWorkPromise = function (job, timer) {
     // 1. 物件 -> new
     return new Promise((resolve, reject) => {
