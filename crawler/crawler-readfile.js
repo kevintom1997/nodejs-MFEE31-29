e83e63f221d0aa7b13a3c98607a991b8b2dfa2fa
kevintom <kk860621kk@gmail.com>
2022-12-11 16:21:36 +0800

    [add crawler-readfile.js]

--- crawler/crawler-readfile.js
+++ crawler/crawler-readfile.js
@@ -4,7 +4,7 @@ const fs = require("fs/promises");
   try {
     let data = await fs.readFile("stock.txt", "utf-8");
     let stockNo = data;
-    let date = '20221111';
+    let date = "20221111";
     let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
       params: {
         stockNo,
@@ -12,7 +12,7 @@ const fs = require("fs/promises");
       },
     });
 
-    console.log('await', response.data);
+    console.log("await", response.data);
   } catch (e) {
     console.error(e);
   }
