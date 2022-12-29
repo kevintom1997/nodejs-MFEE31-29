41ebb8c27a002b5e6c51bfc994632bf0273a8b0f
kevintom <kk860621kk@gmail.com>
2022-12-29 20:17:53 +0800

    [add StockDetail]

--- stock-fe/src/components/StockDetails.js
+++ stock-fe/src/components/StockDetails.js
@@ -1,57 +1,80 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const StockDetails = () => {
   const [error, setError] = useState(null);
+  const { stockId } = useParams();
+  const [Data, setData] = useState([]);
+  // 用 [] ，component 第一次初始化的時候
+  useEffect(() => {
+    async function getData() {
+      let response = await axios.get(
+        `http://localhost:3001/api/stocks/${stockId}`
+      );
+      setData(response.data);
+    }
 
+    getData();
+  }, []);
   return (
     <div>
       {error && <div>{error}</div>}
       <ul>
         <li
           style={{
-            display: 'inline-block',
-            margin: '2px',
+            display: "inline-block",
+            margin: "2px",
             // backgroundColor: page === i ? '#00d1b2' : '',
             // borderColor: page === i ? '#00d1b2' : '#dbdbdb',
             // color: page === i ? '#fff' : '#363636',
-            borderWidth: '1px',
-            width: '28px',
-            height: '28px',
-            borderRadius: '3px',
-            textAlign: 'center',
+            borderWidth: "1px",
+            width: "28px",
+            height: "28px",
+            borderRadius: "3px",
+            textAlign: "center",
           }}
         >
           1
         </li>
       </ul>
       目前在第 1 頁
-      <div className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6">
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">日期：date</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          成交金額：amount
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          成交股數： volume
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          開盤價：open_price
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          收盤價：close_price
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          漲跌價差：delta_price
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          最高價：high_price
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          最低價：low_price
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          成交筆數：transactions
-        </h2>
-      </div>
+      {Data.map((item) => {
+        return (
+          <div
+            className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6"
+            key={item.date}
+          >
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              日期：{item.date}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              成交金額：{item.amount}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              成交股數： {item.volume}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              開盤價：{item.open_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              收盤價：{item.close_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              漲跌價差：{item.delta_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              最高價：{item.high_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              最低價：{item.low_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              成交筆數：{item.transactions}
+            </h2>
+          </div>
+        );
+      })}
     </div>
   );
 };
